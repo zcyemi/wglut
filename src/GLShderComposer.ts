e55679e490c02b7807357cd657b547b22fc65222
Yemi <zcyemi@gmail.com>
2018-09-25 11:40:19 +0800

    ShaderComposer add struct support.

--- src/GLShderComposer.ts
+++ src/GLShderComposer.ts
@@ -21,11 +21,27 @@ export type GLSL_SHADER = GLSL.vs | GLSL.ps;
 export type GLSL_TYPE = GLSL.vec2 | GLSL.vec3 | GLSL.vec4 | GLSL.float | GLSL.int | GLSL.sampler2D | GLSL.void | GLSL.mat3 | GLSL.mat4;
 export type GLSL_PREFIX = GLSL.in | GLSL.out | GLSL.inout;
 export type GLSL_PRECISION = GLSL.lowp | GLSL.mediump | GLSL.highp;
-export type GLSL_PARAM = {type:GLSL_TYPE,symbol:string,prefix?:GLSL_PREFIX}
+export type GLSL_PARAM = {type:GLSL_TYPE,symbol:string,prefix?:GLSL_PREFIX};
 export type GLSL_ATTR = GLSL_PARAM;
 export type GLSL_UNIFORM = {type:GLSL_TYPE,symbol:string};
-export type GLSL_VARY = GLSL_PARAM;
+export type GLSL_VARY = {type:GLSL_TYPE|string,symbol:string,prefix?:GLSL_PREFIX};
 export type GLSL_PRECISION_DEFINE = {type:GLSL_TYPE,level:GLSL_PRECISION};
+
+export class GLSL_STRUCT{
+    public name:string;
+    public parameters:{type:GLSL_TYPE,symbol:string}[] = [];
+    public parameter(t:GLSL_TYPE,name:string){
+        this.parameters.push({type:t,symbol:name});
+    }
+    public mergeCode():string{
+        let source = `struct ${this.name} {`;
+        this.parameters.forEach(p=>{
+            source += ` ${GLSL[p.type]} ${p.symbol};`
+        });
+        source +='};\n'
+        return source;
+    }
+}
 
 export class GLSL_FUNC{
     public name:string;
@@ -75,6 +91,7 @@ export class GLShaderComposerBase<T extends GLShaderComposerBase<T>>{
     protected m_shadertype:GLSL_SHADER;
     protected m_attrs:GLSL_ATTR[]=[];
     protected m_varys:GLSL_VARY[] = [];
+    protected m_varysStruct:GLSL_VARY[] = [];
     protected m_uniform:GLSL_UNIFORM[] = [];
     protected m_precisions:GLSL_PRECISION_DEFINE[] = [];
     protected m_main:GLSL_FUNC= new GLSL_FUNC('main');
@@ -88,8 +105,20 @@ export class GLShaderComposerBase<T extends GLShaderComposerBase<T>>{
         this.m_attrs.push({symbol:sym,type:type});
         return  this.composer;
     }
-    public vary(type:GLSL_TYPE,sym:string,prefix:GLSL_PREFIX):T{
-        this.m_varys.push({type:type,symbol:sym,prefix:prefix});
+    public vary(type:GLSL_TYPE,sym:string,prefix?:GLSL_PREFIX):T{
+        let p = prefix;
+        if(p ==null){
+            p = this.m_shadertype == GLSL.vs? GLSL.out:GLSL.in;
+        }
+        this.m_varys.push({type:type,symbol:sym,prefix:p});
+        return this.composer;
+    }
+    public varyStruct(s:GLSL_STRUCT,sym:string,prefix?:GLSL_PREFIX){
+        let p = prefix;
+        if(p ==null){
+            p = this.m_shadertype == GLSL.vs? GLSL.out:GLSL.in;
+        }
+        this.m_varysStruct.push({type:s.name,symbol:sym,prefix:p});
         return this.composer;
     }
     public uniform(type:GLSL_TYPE,sym:string):T{
@@ -102,6 +131,10 @@ export class GLShaderComposerBase<T extends GLShaderComposerBase<T>>{
     }
     public define(code:string):T{
         this.m_defines.push(code);
+        return this.composer;
+    }
+    public struct(s:GLSL_STRUCT){
+        this.m_defines.push(s.mergeCode());
         return this.composer;
     }
     public main(init:(f:GLSL_FUNC)=>void):T{
@@ -130,21 +163,46 @@ export class GLShaderComposerBase<T extends GLShaderComposerBase<T>>{
         this.m_attrs.forEach(a=>{
             source+= `${GLSL[a.prefix == null? GLSL.in: a.prefix]} ${GLSL[a.type]} ${a.symbol};\n`;
         });
+
         this.m_varys.forEach(v=>{
             let prefix = v.prefix == null?(this.m_shadertype == GLSL.vs? GLSL.out: GLSL.in): v.prefix;
-            source += `${GLSL[prefix]} ${GLSL[v.type]} ${v.symbol};\n`;
+            let vtype:any = v.type;
+            let vstr:string = vtype;
+            if(Number.isInteger(vtype)){
+                vstr = GLSL[vtype];
+            }
+            source += `${GLSL[prefix]} ${vstr} ${v.symbol};\n`;
         });
+
         this.m_uniform.forEach(u=>{
             source += `uniform ${GLSL[u.type]} ${u.symbol};\n`
         })
+
         this.m_defines.forEach(d=>{
             source += `${d}\n`;
         })
+
+        this.m_varysStruct.forEach(v=>{
+            let prefix = v.prefix == null?(this.m_shadertype == GLSL.vs? GLSL.out: GLSL.in): v.prefix;
+            let vtype:any = v.type;
+            let vstr:string = vtype;
+            if(Number.isInteger(vtype)){
+                vstr = GLSL[vtype];
+            }
+            source += `${GLSL[prefix]} ${vstr} ${v.symbol};\n`;
+        });
+
         this.m_funcs.forEach(f=>{
             source += f.mergeCode();
         })
         source += this.m_main.mergeCode();
         this.m_shaderSource = source;
+    }
+
+    public static createStruct(structname:string):GLSL_STRUCT{
+        let s = new GLSL_STRUCT();
+        s.name = structname;
+        return s;
     }
 }
 
