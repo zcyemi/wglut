5191409c3d5e743bce1094810ead3c25493a8d67
yemi <zcyemi@gmail.com>
2018-10-14 15:48:22 +0800

    GLUtility loadImage.

--- src/GLUtility.ts
+++ src/GLUtility.ts
@@ -71,4 +71,18 @@ export class GLUtility{
             xhr.send();
         })
     }
+
+    public static async loadImage(url:string):Promise<HTMLImageElement>{
+        if(url == null) return null;
+        return new Promise<HTMLImageElement>((res,rej)=>{
+            var img =new Image();
+            img.onload = ()=>{
+                res(img);
+            }
+            img.onerror = ()=>{
+                rej('image load failed');
+            };
+            img.src = url;
+        });
+    }
 }
